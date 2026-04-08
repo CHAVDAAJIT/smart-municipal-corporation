5b609d3e2b2c20851fdf3515413aa6f46c1a9e90
CHAVDAAJI <ajitchavda2762@gmail.com>
2026-04-08 17:13:17 +0530

    fix: env variables for production deployment

--- backend/server.js
+++ backend/server.js
@@ -138,6 +138,6 @@ mongoose
   .catch((err) => console.log(err));
 
 /*SERVER START*/
-server.listen(5000, () => {
-  console.log("Server running on port 5000");
+server.listen(process.env.PORT || 5000, () => {
+  console.log("Server running on port " + (process.env.PORT || 5000));
 });
