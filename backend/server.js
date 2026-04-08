9703fdcf11fbb46a83e3b379e185f06f370ca933
CHAVDAAJI <ajitchavda2762@gmail.com>
2026-04-08 14:35:07 +0530

    add api endpoints

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 // ✅ ADD THIS (YOU MISSED THIS PART)
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://smart-municipal-corporation.vercel.app/",
     methods: ["GET", "POST"],
   },
 });
